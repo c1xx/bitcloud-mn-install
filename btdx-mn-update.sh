b572f389d9c0d40330791964f896bda41b87d6dc
c1xx <c1xx@pm.me>
2018-05-15 08:12:00 +0200

    Optimizations

--- btdx-mn-update.sh
+++ btdx-mn-update.sh
@@ -5,19 +5,21 @@
 ##################################################################################
 
 # Variables
+RED_TEXT=`tput setaf 1`
+GREEN_TEXT=`tput setaf 2`
+RESET_TEXT=`tput sgr0`
 CORE_URL=https://github.com/LIMXTEC/Bitcloud/releases/download/2.0.1.0/linux.Ubuntu.16.04.LTS-static-libstdc.tar.gz
 CORE_FILE=linux.Ubuntu.16.04.LTS-static-libstdc.tar.gz
-# If you have not used my Masternode install script, please change path to "bitcloud-cli" file!
+# If you have not used my Masternode install script, please change path to bitcloud-cli & bitcloudd files!
 DATA_PATH=/usr/local/bin
 
 # Stop current running masternode
-echo "*** Stopping Masternode ***"
-echo ""
+echo -n 'Stopping Masternode...'
 $DATA_PATH/bitcloud-cli stop  > /dev/null 2>&1
+sleep 5; echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
 
 # Download current version, extract and copy
-echo "*** Downloading, extracting and copying files ***"
-echo ""
+echo -n 'Downloading, extracting and copying files...'
 cd ~
 wget $CORE_URL > /dev/null 2>&1
 tar -xvf $CORE_FILE > /dev/null 2>&1
@@ -28,31 +30,31 @@ strip bitcloud-tx > /dev/null 2>&1
 cp -f bitcloudd /usr/local/bin > /dev/null 2>&1
 cp -f bitcloud-cli /usr/local/bin > /dev/null 2>&1
 cp -f bitcloud-tx /usr/local/bin > /dev/null 2>&1
+echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
 
 # Delete Files
-echo "*** Deleting unnecessary files ***"
-echo ""
-sleep 5
+echo -n 'Deleting unnecessary files...'
 rm -f ~/$CORE_FILE > /dev/null 2>&1
 rm -f ~/bitcloudd > /dev/null 2>&1
 rm -f ~/bitcloud-cli > /dev/null 2>&1
 rm -f ~/bitcloud-tx > /dev/null 2>&1
 rm -f ~/bitcloud-qt > /dev/null 2>&1
+echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
 
 # Start Masternode running current version
-echo "*** Starting Masternode with current version ***"
-echo ""
+echo -n "Starting Masternode with current version..."
 bitcloudd -daemon > /dev/null 2>&1
-echo "*** Please start you masternode via local desktop wallet debug console -> masternode start-alias YOURMASTERNODEALIAS ***"
-echo ""
+sleep 10; echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
+echo "${RED_TEXT}Please start you masternode via local desktop wallet debug console -> masternode start-alias YOURMASTERNODEALIAS !${RESET_TEXT}"; echo ""
 read -p "After starting your Masternode, press any key to continue... " -n1 -s
+echo -n 'Stopping Masternode...'
 bitcloud-cli stop > /dev/null 2>&1
-sleep 5
+echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
+sleep 5; echo -n 'Starting Masternode again...'
 bitcloudd -daemon > /dev/null 2>&1
+echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
 
 # Show Version and Masternde Info
-echo ""
-echo "*** Masternode Output (after 60 sec wait time) ***"
-echo ""
+echo "Getting Masternode Output (60 sec waiting time...)"
 sleep 60
 bitcloud-cli getinfo
