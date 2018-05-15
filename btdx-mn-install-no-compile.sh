487bd29ed8a8bcad7b59afab914dbeaea4052039
c1xx <c1xx@pm.me>
2018-05-15 12:42:11 +0200

    Optimizations

--- btdx-mn-install-no-compile.sh
+++ btdx-mn-install-no-compile.sh
@@ -4,91 +4,87 @@
 # !! THIS SCRIPT NEED TO RUN AS ROOT !!
 ##################################################################################
 
-cd
+# Variables
+RED_TEXT=`tput setaf 1`
+GREEN_TEXT=`tput setaf 2`
+RESET_TEXT=`tput sgr0`
+CORE_URL=https://github.com/LIMXTEC/Bitcloud/releases/download/2.0.1.0/linux.Ubuntu.16.04.LTS-static-libstdc.tar.gz
+CORE_FILE=linux.Ubuntu.16.04.LTS-static-libstdc.tar.gz
 
-clear
-# declare STRING variable
-STRING1="Make sure you double check before pressing enter! One chance at this only!"
-STRING2="Creating 2GB SWAP File"
-STRING3="Cloning and Compiling Bitcloud Wallet"
-STRING4="Updating system and installing required packages."
-STRING5="Switching to Aptitude"
-STRING6="Some optional installs"
-STRING7="Starting your masternode"
-STRING8="Now, you need to finally start your masternode in the following order:"
-STRING9="Go to your windows/mac wallet and modify masternode.conf as required, then restart and from the Control wallet debug console please enter"
-STRING10="masternode start-alias <mymnalias>"
-STRING11="where <mymnalias> is the name of your masternode alias (without brackets)"
-STRING12="once completed please return to VPS and press the space bar"
-STRING13=""
-
-# print variable on a screen
-echo $STRING1
+echo "Make sure you double check before pressing enter! One chance at this only!"
 
 # Ask for important Data for configuring Masternode
 read -e -p "Server IP Address : " ip
-read -e -p "Masternode Private Key (e.g. 7sQ27dGdwwEGrAPHmfghBBfWZnC6K1rDATNvm986dDfsaw3Wws4 # THE KEY YOU GENERATED EARLIER) : " key
+read -e -p "Masternode Private Key (Generate via local Wallet Debug Console -> masternode genkey) : " key
 read -e -p "Install Fail2ban? [Y/n] : " install_fail2ban
 read -e -p "Install UFW and configure ports? [Y/n] : " UFW
 
-clear
-echo $STRING4
-sleep 10    
+# Update, upgrade Ubuntu and install required packages
+cd ~ > /dev/null 2>&1
+echo -n 'Updating system and installing required packages...'
+sudo apt-get -y update > /dev/null 2>&1
+sudo apt-get -y upgrade > /dev/null 2>&1
+sudo apt-get -y autoremove > /dev/null 2>&1
+sudo apt-get install wget nano htop -y > /dev/null 2>&1
+sudo apt-get install automake build-essential libtool autotools-dev autoconf pkg-config libssl-dev -y > /dev/null 2>&1
+sudo apt-get install libboost-all-dev git npm nodejs nodejs-legacy libminiupnpc-dev redis-server -y > /dev/null 2>&1
+sudo apt-get install software-properties-common -y > /dev/null 2>&1
+sudo apt-get install libevent-dev -y > /dev/null 2>&1
+add-apt-repository ppa:bitcoin/bitcoin -y > /dev/null 2>&1
+apt-get update -y > /dev/null 2>&1
+apt-get install libdb4.8-dev libdb4.8++-dev -y > /dev/null 2>&1
+source ~/.profile > /dev/null 2>&1
+apt-get -y install aptitude > /dev/null 2>&1
+echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
 
-# Create Swap File, update package and upgrade Ubuntu and install required packages
-echo $STRING13
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y autoremove
-sudo apt-get install wget nano htop -y
-sudo apt-get install automake build-essential libtool autotools-dev autoconf pkg-config libssl-dev -y
-sudo apt-get install libboost-all-dev git npm nodejs nodejs-legacy libminiupnpc-dev redis-server -y
-sudo apt-get install software-properties-common -y
-sudo apt-get install libevent-dev -y
-add-apt-repository ppa:bitcoin/bitcoin -y
-apt-get update -y
-apt-get install libdb4.8-dev libdb4.8++-dev -y
-source ~/.profile
-clear
-echo $STRING5
-apt-get -y install aptitude
+# Download Wallet files and copying to /usr/local/bin
+echo -n 'Downloading Wallet files and extracting...'
+cd > /dev/null 2>&1
+mkdir ~/Bitcloud/ > /dev/null 2>&1
+wget $CORE_URL > /dev/null 2>&1
+tar -xvf $CORE_FILE -C ~/Bitcloud/ > /dev/null 2>&1
 
-# Cloning and compiling Bitcloud Wallet
-cd
-mkdir ~/Bitcloud/
-wget https://github.com/LIMXTEC/Bitcloud/releases/download/2.0.1.0/linux.Ubuntu.16.04.LTS-static-libstdc.tar.gz
-tar -xvf linux.Ubuntu.16.04.LTS-static-libstdc.tar.gz -C ~/Bitcloud/
+cd ~/Bitcloud > /dev/null 2>&1
+strip bitcloudd > /dev/null 2>&1
+strip bitcloud-cli > /dev/null 2>&1
+strip bitcloud-tx > /dev/null 2>&1
+cp -f bitcloudd /usr/local/bin > /dev/null 2>&1
+cp -f bitcloud-cli /usr/local/bin > /dev/null 2>&1
+cp -f bitcloud-tx /usr/local/bin > /dev/null 2>&1
+echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
 
-cd ~/Bitcloud
-strip bitcloudd
-strip bitcloud-cli
-strip bitcloud-tx
-cp bitcloudd /usr/local/bin
-cp bitcloud-cli /usr/local/bin
-cp bitcloud-tx /usr/local/bin
+# Delete Files
+echo -n 'Deleting unnecessary files...'
+rm -f ~/$CORE_FILE > /dev/null 2>&1
+rm -rf ~/Bitcloud/ > /dev/null 2>&1
+echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
 
-# Generating Random Passwords
+# Installing, configuring Firewall and Fail2Ban
+if [[ ("$install_fail2ban" == "y" || "$install_fail2ban" == "Y" || "$install_fail2ban" == "") ]]; then
+  echo -n 'Installing and starting Fail2Ban...'
+  cd ~
+  sudo aptitude -y install fail2ban > /dev/null 2>&1
+  sudo service fail2ban restart > /dev/null 2>&1
+  echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
+fi
+if [[ ("$UFW" == "y" || "$UFW" == "Y" || "$UFW" == "") ]]; then
+  echo -n 'Installing and starting Firewall...'
+  sudo apt-get install ufw > /dev/null 2>&1
+  sudo ufw default deny incoming > /dev/null 2>&1
+  sudo ufw default allow outgoing > /dev/null 2>&1
+  sudo ufw allow ssh > /dev/null 2>&1
+  sudo ufw allow 8329/tcp > /dev/null 2>&1
+  sudo echo "y" | sudo ufw enable > /dev/null 2>&1
+  echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
+fi
+
+# Generating Random Passwords, creating bitcloud.conf file
+echo -n 'Creating bitcloud.conf file...'
 password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 password2=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 
-echo $STRING6
-if [[ ("$install_fail2ban" == "y" || "$install_fail2ban" == "Y" || "$install_fail2ban" == "") ]]; then
-  cd ~
-  sudo aptitude -y install fail2ban
-  sudo service fail2ban restart 
-fi
-if [[ ("$UFW" == "y" || "$UFW" == "Y" || "$UFW" == "") ]]; then
-  sudo apt-get install ufw
-  sudo ufw default deny incoming
-  sudo ufw default allow outgoing
-  sudo ufw allow ssh
-  sudo ufw allow 8329/tcp
-  sudo echo "y" | sudo ufw enable
-fi
-
-# Create bitcloud.conf
-sudo mkdir ~/.bitcloud
-sudo touch ~/.bitcloud/bitcloud.conf
+sudo mkdir ~/.bitcloud > /dev/null 2>&1
+sudo touch ~/.bitcloud/bitcloud.conf > /dev/null 2>&1
 echo 'rpcuser='$password'
 rpcpassword='$password2'
 rpcallowip=127.0.0.1
@@ -101,36 +97,16 @@ maxconnections=128
 masternode=1
 masternodeprivkey='$key'
 ' | sudo -E tee ~/.bitcloud/bitcloud.conf >/dev/null 2>&1
-    sudo chmod 0600 ~/.bitcloud/bitcloud.conf
-
-echo 'bitcloud.conf created'
-sleep 40
-
-clear
-echo $STRING4    
+    sudo chmod 0600 ~/.bitcloud/bitcloud.conf > /dev/null 2>&1
+echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
 
 # Starting Bitcloud daemon
-bitcloudd
+echo -n 'Starting Masternode...'
+bitcloudd -daemon > /dev/null 2>&1
+echo "${GREEN_TEXT} OK ${RESET_TEXT}"; echo ""
+echo "${RED_TEXT}Please start you masternode via local desktop wallet debug console -> masternode start-alias YOURMASTERNODEALIAS !${RESET_TEXT}"; echo ""
 
-clear
-echo $STRING4
-sleep 10
-echo $STRING7
-echo $STRING13
-echo $STRING8 
-echo $STRING13
-echo $STRING9 
-echo $STRING13
-echo $STRING10
-echo $STRING13
-echo $STRING11
-echo $STRING13
-echo $STRING12
-sleep 120
-
-cd
-clear
-echo $STRING4    
-
-read -p "(this message will remain for at least 120 seconds) Then press any key to continue... " -n1 -s
+# Show Version and Masternde Info
+echo "Getting Masternode Output (60 sec waiting time...)"
+sleep 60
 bitcloud-cli getinfo
