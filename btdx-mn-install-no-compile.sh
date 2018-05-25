0c9abdedd8d1cc069f126d3ba6842d1e2dce0040
c1xx <c1xx@pm.me>
2018-05-25 12:40:10 +0200

    removed comment

--- btdx-mn-install-no-compile.sh
+++ btdx-mn-install-no-compile.sh
@@ -13,7 +13,7 @@ clear
 
 # Required Ubuntu Version check
 echo -n 'Checking Ubuntu Linux Version...'
-if [[ `lsb_release -rs` == $REQUIRED_UBUNTU_VERSION ]] # replace 8.04 by the number of release you want
+if [[ `lsb_release -rs` == $REQUIRED_UBUNTU_VERSION ]]
 then
 	echo "${GREEN_TEXT} 16.04 OK ${RESET_TEXT}"; echo ""
 else
